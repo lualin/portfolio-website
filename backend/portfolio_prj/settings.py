3b91a4b8cb6e70d7e129b1c361dec2a68eb1b2e5
Sean Kim <ksiw0427@gmail.com>
2023-05-04 05:52:52 +1200

    allowed-hosts added

--- backend/portfolio_prj/settings.py
+++ backend/portfolio_prj/settings.py
@@ -20,72 +20,82 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6x(4&*uzl8bhuk7t=nvj(ivldj4rlby_ylu9x^11v1qe^3u=37'
+SECRET_KEY = "django-insecure-6x(4&*uzl8bhuk7t=nvj(ivldj4rlby_ylu9x^11v1qe^3u=37"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "https://sean-portfolio.onrender.com",
+    "44.226.145.213",
+    "54.187.200.255",
+    "34.213.214.55",
+    "35.164.95.156",
+    "44.230.95.183",
+    "44.229.200.200",
+]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django_admin_index',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'ordered_model',
-    'corsheaders',
-    'rest_framework',    
-    'gallery',
-    'projects',
-    'single_pages'
+    "django_admin_index",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "ordered_model",
+    "corsheaders",
+    "rest_framework",
+    "gallery",
+    "projects",
+    "single_pages",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
-ROOT_URLCONF = 'portfolio_prj.urls'
+ROOT_URLCONF = "portfolio_prj.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['../single_pages/templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": ["../single_pages/templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'portfolio_prj.wsgi.application'
+WSGI_APPLICATION = "portfolio_prj.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -95,16 +105,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -112,9 +122,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'NZ'
+TIME_ZONE = "NZ"
 
 USE_I18N = True
 
@@ -124,16 +134,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/' # URL location of static files located in STATIC_ROOT
-STATICFILES_DIRS = [BASE_DIR / "static"] # Tells Django where to look for static files in a Django Project
-STATIC_ROOT = BASE_DIR / "staticfiles" # Folder location of static files where collectstatic is run
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage" # File storage engine used when collecting static files with the collectstatic command
+STATIC_URL = "/static/"  # URL location of static files located in STATIC_ROOT
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]  # Tells Django where to look for static files in a Django Project
+STATIC_ROOT = (
+    BASE_DIR / "staticfiles"
+)  # Folder location of static files where collectstatic is run
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"  # File storage engine used when collecting static files with the collectstatic command
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Cors header
 
@@ -145,8 +159,8 @@ ADMIN_INDEX_SHOW_REMAINING_APPS_TO_SUPERUSERS = False
 
 if DEBUG:
     MIDDLEWARE = [
-        'whitenoise.middleware.WhiteNoiseMiddleware',
+        "whitenoise.middleware.WhiteNoiseMiddleware",
     ] + MIDDLEWARE
     INSTALLED_APPS = [
-        'whitenoise.runserver_nostatic',
+        "whitenoise.runserver_nostatic",
     ] + INSTALLED_APPS
