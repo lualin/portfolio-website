7ea28288c5712aa1212c0d4140f35c141f816bfa
Sean Kim <ksiw0427@gmail.com>
2023-05-09 02:46:46 +1200

    styles adjusted

--- backend/projects/views.py
+++ backend/projects/views.py
@@ -7,7 +7,7 @@ from .models import Project
 def projectPage(request):
 
     projects = Project.objects.all()
-    
+
     # for project in projects:
     #     stack_list_str = project.stacks
     #     stacks_list = stack_list_str.split(",") if stack_list_str else []
